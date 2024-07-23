537cfbf9dc101eaa24a943c8ffc13d2d13e209f5
MenstraMap <164950661+MenstraMap@users.noreply.github.com>
2024-07-23 19:19:18 -0400

    update

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 // Replace with your Supabase details
 const supabaseUrl = 'https://jmjkclbiybinmgvsnxgw.supabase.co';
